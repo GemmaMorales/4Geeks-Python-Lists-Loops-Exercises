0ccf5435dcf5d2851f5cc7f646291afd476fc297
HammyCakes <gemmaamorales@gmail.com>
2020-04-28 22:55:20 +0000

    second commit

--- exercises/08-Delete_element/app.py
+++ exercises/08-Delete_element/app.py
@@ -1,14 +1,19 @@
 people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 
 #Your code go here:
-def deletePerson(person_name):
-    #people.remove(person_name)
+#def deletePerson(person_name):
+    #for x in people:
+        #if x == person_name:
+            #people.remove(x)
     #return people
+
+def deletePerson(person_name):
+    new_list = []
     for x in people:
-        if x == person_name:
-            people.remove(x)
-    return people
+        if x != person_name:
+            new_list.append(x)
+    return new_list
     
-#print(deletePerson("daniella"))
-#print(deletePerson("juan"))
+print(deletePerson("daniella"))
+print(deletePerson("juan"))
 print(deletePerson("emilio"))
