c872a50c2aa37f9584101fe9344e16ecb3e690a5
GemmaMorales <gemmaamorales@gmail.com>
2020-05-10 02:00:32 +0000

    seventh commit

--- exercises/12.1-more_mapping/app.py
+++ exercises/12.1-more_mapping/app.py
@@ -1,5 +1,16 @@
 myNumbers = [23,234,345,4356234,243,43,56,2]
 
 #Your code go here:
+# def increment_by_one(x):
+#     new_list = []
+#     new_list.append(x * 3)
+#     return new_list
 
-print(new_list)
+# result = list(map(increment_by_one, myNumbers))
+# print(result)
+
+def increment_by_one(x):
+    return x * 3
+
+new_list = list(map(increment_by_one, myNumbers))
+print(new_list)   
